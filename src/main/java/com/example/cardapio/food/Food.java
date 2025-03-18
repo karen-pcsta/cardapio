feec04fee20d842f276e0e6a4f33e94cb573f9dc
Karen <karen.pcosta@gmail.com>
2025-03-18 03:01:52 -0300

    Initial backend config

---
+++ src/main/java/com/example/cardapio/food/Food.java
@@ -0,0 +1,40 @@
+package com.example.cardapio.food;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "foods")
+@Entity(name = "foods")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+
+public class Food {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    private String image; // url da imagem
+
+    private Integer price;
+
+    private String title;
+
+    public Food(FoodRequestDTO data) {
+        this.image = data.image();
+        this.price = data.price();
+        this.title = data.title();
+    }
+
+}
